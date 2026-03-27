7e26006d457edf082e98b8ade6353dabbb6d43b0
Leonardo Flores <leonardo.flores@berzerk.com.br>
2026-03-27 14:43:59 -0300

    Ricing upgrades: wallpapers, hyprlock blur, wofi restyle, waybar MPRIS + power menu, fastfetch, starship cmd duration, Papirus icons

---
+++ .config/hypr/wallpaper-cycle.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Cycle to a random wallpaper with a nice transition
+
+WALLPAPER_DIR="$HOME/.config/hypr/wallpapers"
+
+wallpaper=$(find "$WALLPAPER_DIR" -type f \( -name '*.png' -o -name '*.jpg' \) | shuf -n 1)
+
+swww img "$wallpaper" --transition-type grow --transition-pos 0.5,0.5 --transition-duration 1
---
+++ .config/hypr/wallpaper.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Set a random wallpaper from the collection on login.
+# To change wallpaper manually: Super+W (bound in hyprland.conf)
+
+WALLPAPER_DIR="$HOME/.config/hypr/wallpapers"
+
+sleep 1  # wait for swww-daemon
+
+# Pick a random wallpaper
+wallpaper=$(find "$WALLPAPER_DIR" -type f \( -name '*.png' -o -name '*.jpg' \) | shuf -n 1)
+
+swww img "$wallpaper" --transition-type grow --transition-pos 0.5,0.5 --transition-duration 1
